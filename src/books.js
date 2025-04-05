c7a80a337a436c25cd7093ca1a0f1c919247413f
Eliana Depietri <eliana.depietri@gmail.com>
2025-04-05 17:04:46 +0200

    solved lab no dictionary bonus T_T

--- src/books.js
+++ src/books.js
@@ -38,28 +38,47 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  {title: "The Old Man and the Sea", pages: 128, author: "Ernest Hemingway", details:{language: "English", description:"One of Hemingway's most famous works, it tells the story of Santiago..."}},
+  {title: "The Airbnb Story", pages: 256,  author:"Leight Gallagher", details:{language: "English", description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb"}},
+  {title: "Educated - A Memoir", pages: 352, author: "Tara Westover", details:{language:"English", description: "Educated is an account of the struggle for self-invention..."}},
+  {title: "The Art of Learning", pages:288, author:"Josh Waitzkin", details:{language:"English", description:"The art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."}},
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(object) {
   // Your code here:
 
+  return `${object.title} - ${object.author} - ${object.pages} pages`;
+
+
 }
+
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
 
+  for (let i = 0; i < booksArray.length; i++){
+    delete booksArray[i].details.language
+  }
+   
+console.log(booksArray);
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+booksArray.forEach(function(book){
+  book.readingTime = (Math.ceil(book.pages * 500 / 90));
 
+  })
+  
+  console.log(booksArray);
 
 
 
@@ -86,15 +105,28 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
+function booksByAuthor(dictionary) {
   // Your code here:
-  
+   
 }
+
+// Casi llego ... pero no T_T
+
 
 
 
 // Bonus: Iteration 6 | Average Page Count
-function averagePageCount() {
+function averagePageCount(books) {
   // Your code here:
   
+  let pageTotal= 0;
+
+  books.forEach(function(book){
+    pageTotal += book.pages;
+  
+  })
+  return pageTotal / books.length;
 }
+
+
+
